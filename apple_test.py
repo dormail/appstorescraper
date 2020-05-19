e9af90bb33e414dac223130418d176bf01606f71
Matthias Maile <matthias.maile1@gmail.com>
2020-05-19 10:12:51 +0200

    cli added

--- apple_test.py
+++ apple_test.py
@@ -2,8 +2,15 @@
 from get_applestore_comments import get_applestore_comments
 import pandas as pd
 
+# bvb seite
 url = 'https://apps.apple.com/de/app/borussia-dortmund/id454971210#see-all/reviews'
+
+# tu dortmund app seite
+url = 'https://apps.apple.com/de/app/internet-browser-firefox/id989804926#see-all/reviews'
 
 #url = '/home/matthiasmaile/Documents/sk/appstoescrapper/apple_testcomments.html'
 
-get_applestore_comments(url).to_excel('bvb.xlsx')
+url = input("Type in the URL ")
+appname = input("Type in the apps name ")
+
+get_applestore_comments(url).to_excel(appname + '.xlsx')
