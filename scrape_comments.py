6fd9d74ba77640d42ff10f5a9650832b43e05227
Matthias Maile <matthias.maile1@gmail.com>
2020-05-25 15:24:31 +0200

    added metadata scraper for googleplay

--- scrape_comments.py
+++ scrape_comments.py
@@ -4,6 +4,8 @@
 from get_googleplay_comments import get_googleplay_comments
 from get_applestore_comments import get_applestore_comments
 
+from get_googleplay_appdata import get_googleplay_appdata
+
 import pandas as pd
 
 
@@ -16,16 +18,23 @@ def scrape_comments(data):
     timeout = data[3]
     scroll = data[4]
 
+    # reading downloads needs to be added here
+    google_appdata = get_googleplay_appdata(url_googleplay)
+
     # appending the 'show comments tag'
     url_applestore_com = url_applestore + '#see-all/reviews'
     url_googleplay_com = url_googleplay + '&showAllReviews=true'
 
     df_google = get_googleplay_comments(url_googleplay_com, scroll, timeout)
-    df_apple = get_applestore_comments(url_applestore_com, scroll, timeout)
+    #df_apple = get_applestore_comments(url_applestore_com, scroll, timeout)
 
-    # reading downloads needs to be added here
+    # adding meta data to the dataframes
+    google_downloads = google_appdata[0].replace(",", "")
+    google_rating = google_appdata[1]
+    df_google.iat[0,0] = int(google_downloads)
+    df_google.iat[1,0] = float(google_rating)
 
     df_google.to_excel(appname + '-google.xlsx')
-    df_apple.to_excel(appname + '-apple.xlsx')
+    #df_apple.to_excel(appname + '-apple.xlsx')
 
 # scrape_comments.py end
