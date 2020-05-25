ebc7ea287624de48cdb159a5e531da8f678787e3
Matthias Maile <matthias.maile1@gmail.com>
2020-05-25 15:23:06 +0200

    initial commit

---
+++ get_googleplay_appdata.py
@@ -0,0 +1,44 @@
+# get_googleplay_appdata.py start
+# selenium as a framework for opening websites
+import selenium
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver import ActionChains
+# for timeouts between scrolling down
+import time
+# beatifulSoup for analysing the html code
+from bs4 import BeautifulSoup
+# return in pandas dataframe
+import pandas as pd
+# for removing letters
+from remove_letters import remove_letters
+
+def get_googleplay_appdata(url):
+    result = []
+
+    # loading the page with selenium
+    browser = webdriver.Firefox()
+    browser.get(url)
+    # getting html source code, closing selenium
+    time.sleep(2)
+    html = browser.page_source
+    browser.quit()
+    # loading the html source code into soup
+    soup = BeautifulSoup(html, 'html.parser')
+    
+    # amount of downloads
+    downloads = soup.find("span", class_="AYi5wd TBRnV")
+    downloads = downloads.find("span", class_="").contents[0]
+    #print(downloads)
+    result.insert(0, downloads)
+
+    # average rating
+    rating = soup.find("div", class_="BHMmbe").attrs['aria-label']
+    rating = remove_letters(rating)
+    #print(rating)
+
+    result.insert(1, rating)
+    return result
+
+# get_googleplay_appdata.py end
