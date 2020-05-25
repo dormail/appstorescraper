0af9e47a74f1777d6c27944f463e271ba0093b95
Matthias Maile <matthias.maile1@gmail.com>
2020-05-25 16:01:25 +0200

    scraping average review from apple store

--- scrape_comments.py
+++ scrape_comments.py
@@ -5,6 +5,7 @@ from get_googleplay_comments import get_googleplay_comments
 from get_applestore_comments import get_applestore_comments
 
 from get_googleplay_appdata import get_googleplay_appdata
+from get_applestore_appdata import get_applestore_appdata
 
 import pandas as pd
 
@@ -20,21 +21,27 @@ def scrape_comments(data):
 
     # reading downloads needs to be added here
     google_appdata = get_googleplay_appdata(url_googleplay)
+    apple_appdata = get_applestore_appdata(url_applestore)
 
     # appending the 'show comments tag'
     url_applestore_com = url_applestore + '#see-all/reviews'
     url_googleplay_com = url_googleplay + '&showAllReviews=true'
 
     df_google = get_googleplay_comments(url_googleplay_com, scroll, timeout)
-    #df_apple = get_applestore_comments(url_applestore_com, scroll, timeout)
+    df_apple = get_applestore_comments(url_applestore_com, scroll, timeout)
 
-    # adding meta data to the dataframes
+    # adding meta data to the google df
     google_downloads = google_appdata[0].replace(",", "")
     google_rating = google_appdata[1]
     df_google.iat[0,0] = int(google_downloads)
     df_google.iat[1,0] = float(google_rating)
+    # adding meta data to apple store df
+    apple_downloads = apple_appdata[0]
+    apple_raing = apple_appdata[1]
+    df_apple.iat[0,0] = int(apple_downloads)
+    df_apple.iat[1,0] = float(apple_raing)
 
     df_google.to_excel(appname + '-google.xlsx')
-    #df_apple.to_excel(appname + '-apple.xlsx')
+    df_apple.to_excel(appname + '-apple.xlsx')
 
 # scrape_comments.py end
