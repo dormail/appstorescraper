876342a68720f7319c84db122c09c6b76bc90068
Matthias Maile <matthias.maile1@gmail.com>
2020-05-25 15:24:16 +0200

    lowered scroll number for faster testing

--- scrape_comments-test.py
+++ scrape_comments-test.py
@@ -7,7 +7,7 @@ url_apple = 'https://apps.apple.com/de/app/internet-browser-firefox/id989804926'
 url_google = 'https://play.google.com/store/apps/details?id=com.whatsapp'
 
 timeout = 2
-scroll = 30
+scroll = 2
 
 data = [appname, url_apple, url_google, timeout, scroll]
 
