74717b485675906a5ffee410362352a5d3944221
Matthias Maile <matthias.maile@tu-dortmund.de>
2020-06-15 16:48:52 +0200

    fixed some errors re german tags

--- scrape_comments-test.py
+++ scrape_comments-test.py
@@ -2,12 +2,12 @@
 
 from scrape_comments import scrape_comments
 
-appname = 'whatsapp'
-url_apple = 'https://apps.apple.com/de/app/internet-browser-firefox/id989804926'
-url_google = 'https://play.google.com/store/apps/details?id=com.whatsapp'
+appname = 'anton'
+url_google = 'https://play.google.com/store/apps/details?id=com.solocode.anton&hl=de'
+url_apple = 'https://apps.apple.com/de/app/anton-schule-lernen/id1180554775'
 
 timeout = 2
-scroll = 2
+scroll = 4
 
 data = [appname, url_apple, url_google, timeout, scroll]
 
